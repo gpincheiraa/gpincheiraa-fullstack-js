8ebb9637a0230532ed525920e289993210fb95e2
sbstn-jmnz <sbstn.jmnz@yahoo.com>
2021-04-26 12:40:43 -0400

    chore(dev-scripts): se agregaron scripts de pre-commit y pre-push para asegurar la calidad de la plataforma

---
+++ backend/.eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  "env": {
+    "commonjs": true,
+    "es2021": true,
+    "node": true,
+    "jest/globals": true
+  },
+  "extends": "eslint:recommended",
+  "plugins": [
+    "jest"
+  ],
+  "parserOptions": {
+    "ecmaVersion": 12
+  },
+  "rules": {
+  }
+};
--- backend/src/models/product.js
+++ backend/src/models/product.js
@@ -9,10 +9,10 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
-      // define association here
-    }
-  };
+    // static associate(models) {
+    //   // define association here
+    // }
+  }
   Product.init({
     name: DataTypes.STRING,
     description: DataTypes.STRING,
