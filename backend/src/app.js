3ef4b94d1910da62f5972cbad409ac6d66c9e7e7
Gonzalo Pincheira <g.pincheira.a@gmail.com>
2021-04-27 00:44:24 -0400

    fix pipeline

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const authMiddleware = require('./middleware/auth')
 const routes = require('./routes')
-
+const staticFolder = `${__dirname}/public`
 const app = express()
 
+app.use(express.static(staticFolder))
 app.use('/api', authMiddleware)
 app.use('/api', routes)
-app.use(express.static(`${__dirname}/public`))
+
+app.get('*', (request, response) => {
+  return response.sendFile(`${staticFolder}/index.html`)
+})
 
 module.exports = app
