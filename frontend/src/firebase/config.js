5c5595f0c5e52e951557b2daf0d8a91936a7025a
Gonzalo Pincheira <g.pincheira.a@gmail.com>
2021-04-27 01:17:48 -0400

    fix pipeline

--- frontend/src/firebase/config.js
+++ frontend/src/firebase/config.js
@@ -1,8 +1,8 @@
 export default {
-  apiKey: 'AIzaSyDSWhUXU-76_nW6jFPkNAdWNTPwsfm9_EQ',
-  authDomain: 'javascript-fullstack.firebaseapp.com',
-  projectId: 'javascript-fullstack',
-  storageBucket: 'javascript-fullstack.appspot.com',
-  messagingSenderId: '693714845497',
-  appId: '1:693714845497:web:951103b4d3c7eddf8af2e8'
+  apiKey: 'AIzaSyCWwAJl9zH57SvSQl0V978NIBa-ONbIrJg',
+  authDomain: 'web-developer-js.firebaseapp.com',
+  projectId: 'web-developer-js',
+  storageBucket: 'web-developer-js.appspot.com',
+  messagingSenderId: '60756821751',
+  appId: '1:60756821751:web:a256d5391a11bf58f1b5f2'
 }
